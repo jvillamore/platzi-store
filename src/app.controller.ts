05fb067672b9f4fd52150448f7bac693a070bf4b
Juan Villafuerte Mollinedo <villafuerte.j@outlook.com>
2024-07-25 15:21:53 -0400

    primeras pruebas del controlador, parametros y query params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { of } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,43 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('nuevo')
+  nuevoMetodo() {
+    return 'es un nuevo método';
+  }
+  @Get('/ruta/')
+  nuevaRuta() {
+    return 'es un nuevo método con /';
+  }
+  @Get('products/filter')
+  getProductFilter() {
+    return `products filters`;
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: number) {
+    return `product ${productId}`;
+  }
+  @Get('categories/:id/product/:productId')
+  getCategories(
+    @Param('productId') productId: string,
+    @Param('id') id: string,
+  ) {
+    return `categoria: ${productId}, con idProduct ${id}`;
+  }
+  @Get('products1')
+  getProducts1(@Query() params: any) {
+    // con deconstruccion
+    const { limit, offset } = params;
+    return `productos filtrados limit=> ${limit} y offset=> ${offset}`;
+  }
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('page') page: string,
+  ) {
+    return `Nuevos productos filtrados limit=> ${limit} y offset=> ${offset} y page=> ${page}`;
   }
 }
