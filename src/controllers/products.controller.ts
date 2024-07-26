f017e30a598348d9dbd6f8f2f2eea9ec9892eb54
Juan Villafuerte Mollinedo <villafuerte.j@outlook.com>
2024-07-26 11:02:44 -0400

    creacion de los controladores de products, curstomers, orders y users

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -8,21 +19,48 @@ export class ProductsController {
     @Query('offset') offset = 0,
     @Query('page') page: string,
   ) {
-    return `Nuevos productos filtrados limit=> ${limit} y offset=> ${offset} y page=> ${page}`;
+    return {
+      message: `Nuevos productos filtrados limit=> ${limit} y offset=> ${offset} y page=> ${page}`,
+    };
   }
   @Get('/filter')
   getProductFilter() {
-    return `products filters`;
+    return {
+      message: `products filters`,
+    };
   }
   @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
   getProduct(@Param('productId') productId: number) {
-    return `product --> ${productId}`;
+    return {
+      message: `product --> ${productId}`,
+    };
   }
 
   @Get('products1')
   getProducts1(@Query() params: any) {
     // con deconstruccion
     const { limit, offset } = params;
-    return `productos filtrados limit=> ${limit} y offset=> ${offset}`;
+    return {
+      message: `productos filtrados limit=> ${limit} y offset=> ${offset}`,
+    };
+  }
+  @Post()
+  create(@Body() params: any) {
+    return {
+      message: 'producto creado',
+      params,
+    };
+  }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() params: any) {
+    return {
+      id,
+      params,
+    };
+  }
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return { id };
   }
 }
